2b26bd63b2d4bd97951780bd311fb8961e89ccb8
Takoyaki0805 <ilikemidori0805@gmail.com>
2024-09-24 12:49:13 +0900

    9/24

---
+++ tankbattle/Assets/Objects/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class CameraFollow : NetworkBehaviour
+{
+    GameObject cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cam == null){
+            cam = GameObject.FindWithTag("MainCamera");
+        }
+        if(IsOwner){
+            cam.transform.position = this.transform.position + Vector3.back*3 + Vector3.up*2;
+        }
+    }
+}
